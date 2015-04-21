8887ea9997028962c1c64714b2a9da09a7f5b81c
Wyatt Pan <wppurking@gmail.com>
2015-04-22 00:51:10 +0800

    * es6 化.

--- front/app/routes/todos.js
+++ front/app/routes/todos.js
@@ -1,13 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model: function() {
+	model() {
 		return this.store.findAll('todo');
-	},
-
-	actions: {
-		error: function(error, tran) {
-			console.log("In Todos Route..");
-		}
 	}
 });
