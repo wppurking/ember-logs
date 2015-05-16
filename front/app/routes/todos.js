1c7094fc3d98f0c863499d0e48bd6f5021ebde93
Wyatt Pan <wppurking@gmail.com>
2015-05-16 12:18:24 +0800

    make jshintrc happy

--- front/app/routes/todos.js
+++ front/app/routes/todos.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import AuthRouter from '../mixins/auth-router'
+import AuthRouter from '../mixins/auth-router';
 
 export default Ember.Route.extend(AuthRouter, {
   model() {
